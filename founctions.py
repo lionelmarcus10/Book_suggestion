069aa1c0acaa67cc08e43922e0916e070f10d184
lionelmarcus10 <toglanlionel@gmail.com>
2021-11-22 12:07:11 +0100

    s3

--- founctions.py
+++ founctions.py
@@ -1,24 +1,24 @@
 def ask_pseudonyme():
-    ask_pseudonyme=str(input("Psedudonyme : "))
+    ask_pseudonyme = str(input("Psedudonyme : "))
     return ask_pseudonyme
 def ask_genre():
     print("1 - HOMME ")
     print("2 - FEMME ")
     print("3 - PEU IMPORTE ")
-    ask_genre=int(input("Qullez votre genre ?  "))
-    while (ask_genre != 1 and ask_genre != 2 and ask_genre != 3 ) :
+    ask_genre=(input("Qullez votre genre ?  "))
+    while (ask_genre != '1' and ask_genre != '2' and ask_genre != '3' ) :
         print("1 - HOMME ")
         print("2 - FEMME ")
         print("3 - PEU IMPORTE ")
-        ask_genre = int(input("Quel votre genre ?  "))
+        ask_genre = (input("Quel votre genre ?  "))
     return ask_genre
 
 def ask_age():
     print("1 - <= 18 ans")
     print("2 - Entre 18 ans et 25 ans")
     print('3 - > 25 ans ')
-    ask_age = int(input("Quel est votre âge ? "))
-    while (ask_age != 1 and ask_age != 2 and ask_age != 3 ) :
+    ask_age = (input("Quel est votre âge ? "))
+    while (ask_age != '1' and ask_age != '2' and ask_age != '3' ) :
         print("1 - <= 18 ans")
         print("2 - Entre 18 ans et 25 ans")
         print('3 - > 25 ans ')
@@ -32,17 +32,17 @@ def ask_style_de_lecture():
     ask_style_de_lecture = int(input("Saisir votre style de lecture : "))
     while (ask_style_de_lecture < 1 or ask_style_de_lecture > 7):
         ask_style_de_lecture = int(input("Saisir votre style de lecture : "))
-    print(ask_style_de_lecture)
-    return ask_style_de_lecture
+
+    return str(ask_style_de_lecture)
 
 def ask_menu():
     print("1 - Profils des lecteurs")
     print("2 - Visiter le dépôt des livres")
     print("3 - Recommandation")
-    choix_menu = int(input(" Que voulez vous visiter ? "))
-    while ( choix_menu < 1 or choix_menu > 3):
-        choix_menu = int(input(" Que voulez vous visiter ? "))
-    return choix_menu
+    choix_menu = (input(" Que voulez vous visiter ? "))
+    while (( choix_menu !='1' and choix_menu !='3' and choix_menu != '2')):
+        choix_menu = (input(" Que voulez vous visiter ? "))
+    return int(choix_menu)
 
 def ask_menu_profil():
     print("1 - Ajouter un lecteur")
@@ -55,7 +55,7 @@ def ask_menu_profil():
     return profil_menu
 
 def ask_menu_dépot_livres():
-    print("1 - Afficher la liste des livres dans le dépôt (liste initiale fournie dans le fichier books.txt) \n "
+    print("1 - Afficher la liste des livres dans le dépôt  \n "
           "2 - Ajouter un livre au dépôt \n 3 - Modifier le titre d’un livre dans le dépôt \n 4 - Supprimer un livre du dépôt ")
     dépot_menu = int(input(" Que voulez vous faire ? "))
     while (dépot_menu < 1 or dépot_menu > 4):
@@ -70,9 +70,21 @@ def ask_recommandation_livre():
 
 
 def ajouter_un_lecteur():
-    numero_pseudo = ask_pseudonyme()
+    nom_pseudo = ask_pseudonyme()
     numero_genre = ask_genre()
     numero_age = ask_age()
     numero_style_lecture = ask_style_de_lecture()
+    with open("readers.txt","a") as readers_db:
+        readers_db.write(nom_pseudo +"," + numero_genre + ','+ numero_age +','+ numero_style_lecture+ '\n')
+def supprimer_un_lecteur():
+    name_a_sup= ask_pseudonyme()
+    with open("readers.txt","r") as readers_db:
+        sup_lecteur= readers_db.readlines()
+        for lines in sup_lecteur:
+            if name_a_sup in lines :
+                del lines
+                return ('Lecteur {} supprimé'.format(name_a_sup))
+
+
 
 
--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from founctions import *
 #appeler le menu de lancement
 menu = ask_menu()
 if menu ==1:
-    ask_menu_profil()
+    menu_profile_asked = ask_menu_profil()
+    if menu_profile_asked == 1:
+        ajouter_un_lecteur()
+    if menu_profile_asked == 4:
+        supprimer_un_lecteur()
 elif menu == 2:
     ask_menu_dépot_livres()
 elif menu == 3:
     ask_recommandation_livre()
+
