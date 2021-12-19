f7156c54528e770f6bd15261f1945f9ef9637a89
lionelmarcus10 <93621149+lionelmarcus10@users.noreply.github.com>
2021-12-19 21:09:53 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from autres_fonctions import  explication_processus
-from menu import menu_de_lancement
-
-#appeler la fonction qui donne des consignes a suivre
-explication_processus()
-
-#appeler le menu de lancement
-menu_de_lancement()
